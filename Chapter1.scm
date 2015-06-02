688fe4b62de2e8df6c870098d34cf76a278aa2e9
middleware <middle.fengdong@gmail.com>
2015-06-01 21:35:54 -0700

    n-th element func.

--- Chapter1.scm
+++ Chapter1.scm
@@ -16,3 +16,11 @@
 				(begin (in-S? (- n 3)))
 				(begin #f)))))
 
+
+(define nth-element
+	(lambda (lst n)
+		(if (null? lst)
+			(display 'list too short')
+			(if (zero? n)
+				(car lst)
+				(nth-element (cdr lst) (- n 1)) ) ) ) )
