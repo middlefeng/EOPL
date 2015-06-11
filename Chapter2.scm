5eeb577397098cde5f65a4e963f19614f4f4d3f7
Dong Feng <dfeng@adobe.com>
2015-06-10 17:27:35 -0700

    Procedural Environment.

--- Chapter2.scm
+++ Chapter2.scm
@@ -6,3 +6,23 @@
 		(successor (plus (predecessor x) y)) ))
 
 
+
+; Procedural Representation of Enviornment
+
+(define empty-env
+	(lambda () 
+		(lambda (search-var)
+			(display "can not find") )))
+
+
+(define (extend-env saved-var saved-val saved-env)
+	(lambda (search-var)
+		(if (eq? search-var saved-var)
+			saved-val
+			(saved-env search-var) )))
+
+
+(define (apply-env env search-var)
+	(env search-var))
+
+
